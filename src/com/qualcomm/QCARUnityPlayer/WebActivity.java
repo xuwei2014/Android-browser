e8a21bd169642c6db2fa9f65b2870cd52796088d
Wei Xu <stephenxu412@gmail.com>
2015-02-07 12:03:22 +0800

    modify the backPressed and send messaget to Unity

--- src/com/qualcomm/QCARUnityPlayer/WebActivity.java
+++ src/com/qualcomm/QCARUnityPlayer/WebActivity.java
@@ -2,6 +2,8 @@ package com.qualcomm.QCARUnityPlayer;
 
 import com.qualcomm.Utils.FavoritesManager;
 
+import com.unity3d.player.UnityPlayer;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -21,98 +23,100 @@ import android.widget.Toast;
 
 public class WebActivity extends Activity {
 	private final String DEB_TAG = "WebActivity_debug";
-	
+
 	String curUrl = "";
 	String curTitle = "";
 	WebView mWebView;
 	FavoritesManager favManager;
-	
+
 	// Progress Bar
 	private ProgressBar progressBar;
-	
+
 	// Buttons
 	private Button preButton;
 	private Button nextButton;
 	private Button windowButton;
-	private Button toolsButton;	
+	private Button toolsButton;
 	private Button homeButton;
-	
+
 	// Listeners
 	private ButtonClickedListener buttonListener;
 
-    @SuppressLint("SetJavaScriptEnabled")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_PROGRESS);
-        setContentView(R.layout.webview);
-        
-        progressBar = (ProgressBar)findViewById(R.id.web_progress_bar);
-        
-        mWebView = (WebView)findViewById(R.id.webview);
-        preButton = (Button)findViewById(R.id.pre_button);
-        nextButton = (Button)findViewById(R.id.next_button);
-        windowButton = (Button)findViewById(R.id.window_button);
-        toolsButton = (Button)findViewById(R.id.tools_button);
-        homeButton = (Button)findViewById(R.id.home_button);
-        
-        progressBar.setVisibility(View.GONE);
-        
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl(this.getIntent().getStringExtra("url"));
-        mWebView.setWebViewClient(new MyWebViewClient());
-        mWebView.setWebChromeClient(new MyChromeClient());
-        
-        favManager = new FavoritesManager(getApplicationContext());
-        
-        buttonListener = new ButtonClickedListener();
-        
-        preButton.setOnClickListener(buttonListener);
-        nextButton.setOnClickListener(buttonListener);
-        windowButton.setOnClickListener(buttonListener);
-        toolsButton.setOnClickListener(buttonListener);
-        homeButton.setOnClickListener(buttonListener);
-        
-        preButton.setEnabled(false);
-        nextButton.setEnabled(false);
-    }
-      
-    private class MyWebViewClient extends WebViewClient {
-    	@Override
-    	public boolean shouldOverrideUrlLoading(WebView view, String url) {
-    		view.loadUrl(url);
-    		return true;
-    	}
-    	
-    	@Override
-    	public void onPageFinished(WebView view, String url) {
-    		super.onPageFinished(view, url);
-    		WebActivity.this.curUrl = url;
-    		preButton.setEnabled(mWebView.canGoBack());
-    		nextButton.setEnabled(mWebView.canGoForward());
-    	}    	
-    }
-    
-    private class MyChromeClient extends WebChromeClient {
-    	@Override
-    	public void onReceivedTitle(WebView view, String title) {
-    		super.onReceivedTitle(view, title);
-    		curTitle = title;
-    	}
-    	
-    	@Override
-    	public void onProgressChanged(WebView view, int newProgress) {
-    		super.onProgressChanged(view, newProgress);
-    		if (newProgress == 100) {
-    			progressBar.setVisibility(View.GONE);
-    		} else {
-    			progressBar.setVisibility(View.VISIBLE);
-    			progressBar.setProgress(newProgress);
-    		}
-    	}
-    }
-    
-    private class ButtonClickedListener implements OnClickListener {
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_PROGRESS);
+		setContentView(R.layout.webview);
+
+		progressBar = (ProgressBar) findViewById(R.id.web_progress_bar);
+
+		mWebView = (WebView) findViewById(R.id.webview);
+		preButton = (Button) findViewById(R.id.pre_button);
+		nextButton = (Button) findViewById(R.id.next_button);
+		windowButton = (Button) findViewById(R.id.window_button);
+		toolsButton = (Button) findViewById(R.id.tools_button);
+		homeButton = (Button) findViewById(R.id.home_button);
+
+		progressBar.setVisibility(View.GONE);
+
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.getSettings().setUseWideViewPort(true);
+		mWebView.getSettings().setLoadWithOverviewMode(true);
+		mWebView.loadUrl(this.getIntent().getStringExtra("url"));
+		mWebView.setWebViewClient(new MyWebViewClient());
+		mWebView.setWebChromeClient(new MyChromeClient());
+
+		favManager = new FavoritesManager(getApplicationContext());
+
+		buttonListener = new ButtonClickedListener();
+
+		preButton.setOnClickListener(buttonListener);
+		nextButton.setOnClickListener(buttonListener);
+		windowButton.setOnClickListener(buttonListener);
+		toolsButton.setOnClickListener(buttonListener);
+		homeButton.setOnClickListener(buttonListener);
+
+		preButton.setEnabled(false);
+		nextButton.setEnabled(false);
+	}
+
+	private class MyWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			super.onPageFinished(view, url);
+			WebActivity.this.curUrl = url;
+			preButton.setEnabled(mWebView.canGoBack());
+			nextButton.setEnabled(mWebView.canGoForward());
+		}
+	}
+
+	private class MyChromeClient extends WebChromeClient {
+		@Override
+		public void onReceivedTitle(WebView view, String title) {
+			super.onReceivedTitle(view, title);
+			curTitle = title;
+		}
+
+		@Override
+		public void onProgressChanged(WebView view, int newProgress) {
+			super.onProgressChanged(view, newProgress);
+			if (newProgress == 100) {
+				progressBar.setVisibility(View.GONE);
+			} else {
+				progressBar.setVisibility(View.VISIBLE);
+				progressBar.setProgress(newProgress);
+			}
+		}
+	}
+
+	private class ButtonClickedListener implements OnClickListener {
 		@Override
 		public void onClick(View v) {
 			switch (v.getId()) {
@@ -121,26 +125,30 @@ public class WebActivity extends Activity {
 					mWebView.goBack();
 				}
 				break;
-				
+
 			case R.id.next_button:
 				if (mWebView.canGoForward()) {
 					mWebView.goForward();
 				}
 				break;
-				
+
 			case R.id.window_button:
-				Log.d(DEB_TAG, "add favorites: title: " + curTitle + ", url: " + curUrl);
+				Log.d(DEB_TAG, "add favorites: title: " + curTitle + ", url: "
+						+ curUrl);
 				if (favManager.addFavorite(curTitle, curUrl)) {
-					Toast.makeText(WebActivity.this, "书签保存成功", Gravity.BOTTOM).show();
+					Toast.makeText(WebActivity.this, "书签保存成功", Gravity.BOTTOM)
+							.show();
 				}
 				break;
-				
+
 			case R.id.tools_button:
 				Log.d(DEB_TAG, "open favorites activity");
-				startActivityForResult(new Intent(WebActivity.this, FavActivity.class), 0);
+				startActivityForResult(new Intent(WebActivity.this,
+						FavActivity.class), 0);
 				break;
-				
+
 			case R.id.home_button:
+				UnityPlayer.UnitySendMessage("Main Camera", "ExitWebScene", "");
 				finish();
 				break;
 
@@ -148,20 +156,25 @@ public class WebActivity extends Activity {
 				break;
 			}
 		}
-    }
-       
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-    	if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-    		mWebView.goBack();
-    		return true;
-    	}
-    	return super.onKeyDown(keyCode, event);
-    }
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	switch (resultCode) {
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (mWebView.canGoBack()) {
+				mWebView.goBack();
+			} else {
+				UnityPlayer.UnitySendMessage("Main Camera", "ExitWebScene", "");
+				finish();
+			}
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (resultCode) {
 		case 0:
 			Log.d(DEB_TAG, "favorites get url: " + data.getStringExtra("url"));
 			mWebView.loadUrl(data.getStringExtra("url"));
@@ -170,7 +183,7 @@ public class WebActivity extends Activity {
 		default:
 			break;
 		}
-    	super.onActivityResult(requestCode, resultCode, data);
-    }
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 
 }
