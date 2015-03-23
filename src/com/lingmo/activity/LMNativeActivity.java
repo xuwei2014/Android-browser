86e47af1b9c13f1c3819ec7c3b5ad35fe9cb6a39
Wei Xu <stephenxu412@gmail.com>
2015-03-23 10:06:47 +0800

    add the ShareSDK initial in the OnCreate

--- src/com/lingmo/activity/LMNativeActivity.java
+++ src/com/lingmo/activity/LMNativeActivity.java
@@ -1,10 +1,18 @@
 package com.lingmo.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
+
+import cn.sharesdk.unity3d.ShareSDKUtils;
 
 import com.unity3d.player.UnityPlayerNativeActivity;
 
 public class LMNativeActivity extends UnityPlayerNativeActivity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ShareSDKUtils.prepare(this.getApplicationContext());
+	}
 	
 	public void openWeb(String url, boolean straight) {
 		Intent intent = new Intent(this, WebActivity.class);
